35ee4fd586e9c8caaa3e48003c1c64eeb56b8a96
roman <romissevd@gmail.com>
2021-02-28 19:16:39 +0200

    remove url

--- directory/urls.py
+++ directory/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('category/', views.CategoryView.as_view(), name='category'),
-    path('category/add/', views.CreateCategoryView.as_view(), name='add_category')
 ]
